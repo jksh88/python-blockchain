737d61e99efd3ae68d52d8637ec71665f7408c38
Joohoon Kim <jayjhkim7@gmail.com>
2021-02-17 23:58:28 -0500

    improved the hashing function

--- block.py
+++ block.py
@@ -1,4 +1,5 @@
 import time
+from crypto_hash import crypto_hash
 
 class Block:
   '''
@@ -26,7 +27,8 @@ class Block:
     '''
     timestamp = time.time_ns()
     last_hash = last_block.hash
-    hash = f'{timestamp}-{last_hash}'
+    # hash = f'{timestamp}-{last_hash}'
+    hash = crypto_hash(timestamp, last_hash, data)
 
     return Block(timestamp, last_hash, hash, data)
 
--- crypto_hash.py
+++ crypto_hash.py
@@ -1,15 +1,20 @@
 import hashlib
 import json
 
-def crypto_hash(data):
+
+def crypto_hash(*args):
   '''
-  Return a sha-256 hash of the given data
+  Return a sha-256 hash of the given arguments
   '''
-  stringified_data = json.dumps(data)
-  return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+  stringified_args = sorted(map(lambda data: json.dumps(data) , args))
+  joined_data = ''.join(stringified_args)
+  # print(f'stringified_args: {args}')
+  # print(f'joined_data: {joined_data}')
+  return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
-  print(f"cryto_hash([1, 2]): {crypto_hash([1, 2])}")
+  print(f"cryto_hash('one', 2, [3]): {crypto_hash('one', 2, [3])}")
+  print(f"crypto_hash([3], 2,'one'): {crypto_hash([3], 2, 'one')}")
 
 if __name__ == '__main__':
   main()
