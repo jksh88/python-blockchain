3b93ace1574512a019359a6c5d40ab06bebe1acb
Joohoon Kim <jayjhkim7@gmail.com>
2021-04-19 23:36:36 -0400

    proof of work setup

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -5,19 +5,22 @@ GENESIS_DATA = {
   'timestamp': 1,
   'last_hash': 'genesis_last_hash',
   'hash': 'genesis_hash',
-  'data': []
-
+  'data': [],
+  'difficulty': 3,
+  'nonce': 'genesis_nonce'
 }
 
 class Block:
   '''
   block: a unit of storage
   '''
-  def __init__(self, timestamp, last_hash, hash, data):
+  def __init__(self, timestamp, last_hash, hash, data, difficulty, nonce):
     self.timestamp = timestamp
     self.last_hash = last_hash
     self.hash = hash
     self.data = data
+    self.difficulty = difficulty
+    self.nonce = nonce
 
   def __repr__(self):
     return (
@@ -25,20 +28,29 @@ class Block:
       f'timestamp: {self.timestamp}, '
       f'last_hash: {self.last_hash}, '
       f'hash: {self.hash}, '
-      f'data: {self.data})'
+      f'data: {self.data}, '
+      f'difficulty: {self.difficulty}, '
+      f'nonce: {self.nonce})'
     )
 
   @staticmethod
   def mine_block(last_block, data):
     '''
-    mines a block based on the given last_block and data
+    mines a block based on the given last_block and data until a block has is found that meets the leading 0's proof of work requirement.
     '''
     timestamp = time.time_ns()
     last_hash = last_block.hash
+    difficulty = last_block.difficulty
+    nonce = 0
     # hash = f'{timestamp}-{last_hash}'
-    hash = crypto_hash(timestamp, last_hash, data)
+    hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
-    return Block(timestamp, last_hash, hash, data)
+    while hash[0:difficulty] != '0' * difficulty:
+      nonce += 1
+      timestamp = time.time_ns()
+      hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
+
+    return Block(timestamp, last_hash, hash, data, difficulty, nonce)
 
   @staticmethod
   def genesis():
@@ -47,6 +59,7 @@ class Block:
     '''
     # return Block(timestamp=GENESIS_DATA['timestamp'], last_hash=GENESIS_DATA['last_hash'], hash=GENESIS_DATA['hash'], data=GENESIS_DATA['data'])
     return Block(**GENESIS_DATA)
+    # '*' is an unpacking operator. Here, they are used to automatically apply the difficulty and nonce values from the GENESIS_DATA dictionary to this creation of Block instance.
 
 def main():
   genesis_block = Block.genesis()
