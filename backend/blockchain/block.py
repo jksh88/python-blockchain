53167a3dfe1c7b0b27594e840ecebeb1893ec886
Joohoon Kim <jayjhkim7@gmail.com>
2021-04-23 23:30:23 -0400

    dynamically adjust difficulty

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,5 +1,6 @@
 import time
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
   'timestamp': 1,
@@ -40,7 +41,7 @@ class Block:
     '''
     timestamp = time.time_ns()
     last_hash = last_block.hash
-    difficulty = last_block.difficulty
+    difficulty = Block.adjust_difficulty(last_block, timestamp)
     nonce = 0
     # hash = f'{timestamp}-{last_hash}'
     hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
@@ -48,6 +49,7 @@ class Block:
     while hash[0:difficulty] != '0' * difficulty:
       nonce += 1
       timestamp = time.time_ns()
+      difficulty = Block.adjust_difficulty(last_block, timestamp)
       hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
     return Block(timestamp, last_hash, hash, data, difficulty, nonce)
@@ -60,6 +62,21 @@ class Block:
     # return Block(timestamp=GENESIS_DATA['timestamp'], last_hash=GENESIS_DATA['last_hash'], hash=GENESIS_DATA['hash'], data=GENESIS_DATA['data'])
     return Block(**GENESIS_DATA)
     # '*' is an unpacking operator. Here, they are used to automatically apply the difficulty and nonce values from the GENESIS_DATA dictionary to this creation of Block instance.
+
+  @staticmethod
+  def adjust_difficulty(last_block, new_timestamp):
+    """
+    Calculate the adjusted dificulty according to the MINE_RATe.
+    Increase the difficulty for quickly mined blocks.
+    Decrease the difficulty for slowly mined blocks.
+    """
+    if MINE_RATE > (new_timestamp - last_block.timestamp):
+      return last_block.difficulty + 1
+    
+    if (last_block.difficulty -1) > 0:
+      return last_block.difficulty - 1 
+    
+    return 1
 
 def main():
   genesis_block = Block.genesis()
